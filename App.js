61436f38de92205aca53771bf6d7cf6122d58975
jacksonPeaku <jackson.avila@peaku.ai>
2023-03-22 23:24:30 -0500

    init app

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import Expo from "expo";
+import * as THREE from "three";
 
-export default function App() {
+import ExpoTHREE, { Renderer } from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
+
+
+const App = () => {
+
+  const onContextCreate = async (gl) => {
+    //THREE.js code
+
+    //GET scene
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      gl.drawingBufferWidth / gl.drawingBufferHeight,
+      0.1,
+      1000
+    )
+
+    gl.canvas = { with: gl.drawingBufferWidth, height: gl.drawingBufferHeight }
+    camera.position.z = 2
+
+    const renderer = new Renderer({ gl })
+    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight)
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1)
+    const material = new THREE.MeshBasicMaterial({
+      color: 'blue'
+    })
+    const cube = new THREE.Mesh(geometry, material)
+    scene.add(cube)
+
+
+    const render = () => {
+      requestAnimationFrame(render)
+
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+      renderer.render(scene, camera)
+      gl.endFrameEXP()
+    }
+
+    render()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <GLView
+        onContextCreate={onContextCreate}
+        style={{ with: 500, height: 500 }}
+      />
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
